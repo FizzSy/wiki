c5f0467a0b33219fc81656287ff1cd710129e331
lyw <1369530215@qq.com>
2019-12-06 16:16:19 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 git push -f git@github.com:FizzSy/luyuwen.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:FizzSy/documents.git master:gh-pages
+# git push git@github.com:FizzSy/documents.git master:gh-pages
 
 cd -
